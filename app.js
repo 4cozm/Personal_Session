520b80621518459f99dbceb52381eb6beb155f6d
4cozm <17328rm@gmail.com>
2024-05-16 14:11:56 +0900

    add:items 생성 추가

--- app.js
+++ app.js
@@ -1,23 +1,22 @@
 import express from 'express';
 import { MongoClient } from 'mongodb';
-
+import mongoose from 'mongoose';
 const mongodb_password = process.env.mongodb_password;
 let db; //데이터베이스 연결 저장하는 전역변수
 const playerCollections = db.collection('player');
 await playerCollections.createIndex({ name: 1 }, { unique: true }); //이름에 고유 인덱스 부여,중복되지 않도록 보장
 const itemCollections = db.collection('item');
+await itemCollections.createIndex({ item_name: 1 }, { unique: true });
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', async (req, res) => {});
-
 app.post('/api/characters', async (req, res) => {
   if (!req.body.name)
     return res.status(400).json({ error: '캐릭터 이름을 입력해주세요' });
- let lastId = await playerCollections.findOne({}, { sort: { _id: -1 } });
-  if (lastId == null) lastId = 1;
+  let lastId = await playerCollections.findOne({}, { sort: { _id: -1 } });
+  if (lastId == null) lastId = 0;
   const newPlayer = {
     character_id: lastId + 1, //가장 최근 생성된 캐릭터의 ID값에 +1을 하여 생성
     name: req.body.name,
@@ -39,17 +38,50 @@ app.delete('/api/characters/:id', async (req, res) => {
     res.status(500).json({ error: '캐릭터 삭제 중 오류 발생' });
   }
 });
-app.get('/api/characters/:id',async (req,res)=>{
-    const characterId = req.params.id;
-    try {
-        const find = await playerCollections.findOne({ character_id: characterId });
-        if(!find)
-            return res.status(404).json({error:"해당 ID를 가진 캐릭터가 존재하지 않습니다"});
-        res.json(find);
-      } catch (error) {
-        res.status(500).json({ error: '캐릭터 검색 중 오류 발생' });
-      }
-})
+app.get('/api/characters/:id', async (req, res) => {
+  const characterId = req.params.id;
+  try {
+    const find = await playerCollections.findOne({ character_id: characterId });
+    if (!find)
+      return res
+        .status(404)
+        .json({ error: '해당 ID를 가진 캐릭터가 존재하지 않습니다' });
+    res.json(find);
+  } catch (error) {
+    res.status(500).json({ error: '캐릭터 검색 중 오류 발생' });
+  }
+});
+
+const itemSchema = new mongoose.Schema({
+  item_code: { type: Number, required: true },
+  item_name: { type: String, required: true },
+  item_stat: {
+    health: { type: Number, required: true },
+    power: { type: Number, required: true },
+  },
+});
+const itemModel = mongoose.model('item', itemSchema);
+
+app.post('/api/items', async (req, res) => {
+  try {
+    let lastItemId = await itemCollections.findOne({}, { sort: { _id: -1 } });
+    if (lastItemId == null) {
+      lastItemId = 0;
+    }
+    const { item_name, item_stat } = req.body;
+    const item_code = lastItemId +1;
+
+    const newItem = await itemModel.create({
+        item_code,
+        item_name,
+        item_stat
+    });
+    res.status(201).json(newItem);
+
+  } catch {
+    res.status(500).json({error:"서버오류 발생"});
+  }
+});
 
 async function startServer() {
   try {
